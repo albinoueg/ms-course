fde4361430121a3d5d43e56111d5bb5fb4b8e949
Albino Moreira <albino.sistemas@gmail.com>
2021-06-24 19:48:42 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/br/com/albinomoreira/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/albinomoreira/hrworker/resources/WorkerResource.java
@@ -35,7 +35,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
-		
+
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker obj = repository.findById(id).get();
