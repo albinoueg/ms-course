aa9befe3eb1a3edfa412fad3c09d03e49bb3d9a2
Albino Moreira <albino.sistemas@gmail.com>
2021-06-27 00:31:13 -0300

    Login e geração de token JWT

--- hr-oauth/src/main/java/br/com/albinomoreira/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/albinomoreira/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package br.com.albinomoreira.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.albinomoreira.hroauth.entities.User;
 import br.com.albinomoreira.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,18 @@ public class UserService {
 			throw new IllegalArgumentException("Email não encontrado");
 		}
 		logger.error("Email encontrado: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if(user == null) {
+			logger.error("Email não encontrado: " + username);
+			throw new UsernameNotFoundException("Email não encontrado");
+		}
+		logger.error("Email encontrado: " + username);
 		return user;
 	}
 
